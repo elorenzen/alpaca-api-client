229b41af7a0cddbc98567162a9aeacf86caa6b9a
Eric <eric@kellyklee.com>
2021-05-19 10:33:46 -0700

    Create simple method to buy 100 shares of  on 'q-btn' click

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,32 @@
 <template>
   <q-page class="flex flex-center">
     {{ accountData }}
+
+    <q-btn class="col-12" label="Buy 100 $NET shares" size="lg" outline @click="buyCloudflare" />
+
     <PositionsList :positions="positions" :orders="orders" />
   </q-page>
 </template>
 
 <script>
 import PositionsList from 'src/components/PositionsList'
+import { alpacaInstance } from 'boot/alpaca-api'
 
 export default {
   props: ['accountData', 'positions', 'orders'],
   components: { PositionsList },
   name: 'Home',
+  methods: {
+    async buyCloudflare () {
+      await alpacaInstance.createOrder({
+        symbol: 'NET', // any valid ticker symbol
+        qty: 100,
+        side: 'buy',
+        type: 'market',
+        time_in_force: 'day'
+      })
+      window.location.reload()
+    }
+  }
 };
 </script>
